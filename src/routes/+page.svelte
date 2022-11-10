6273e294b03dcd0928a9b77f48ea2b00bcaa2c10
Sebastian <swebermorales@gmail.com>
2022-11-09 19:16:55 -0800

    Inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form name="netlify-form-test" method="POST" netlify-honeypot="nbot-field" data-netlify="true">
+    <input type="hidden" name="form-name" value="netlify-form-test"/>
+    <label for="name">Name</label>
+    <input name="name" id="name" required placeholder="Name" type="text"/> 
+    <label for="email">Email</label>
+    <input name="email" id="email" required placeholder="Email" type="text"/> 
+    <input type="submit" value="Submit"/>
+</form>
